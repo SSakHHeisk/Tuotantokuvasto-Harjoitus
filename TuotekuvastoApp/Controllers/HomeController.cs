4d96e4346c9b0a9054b4ce96efab0b180ab91bbe
Saku Heiskanen <2307392@edu.karelia.fi>
2024-10-31 11:25:00 +0200

    Tuotejutska lisätty

--- TuotekuvastoApp/Controllers/HomeController.cs
+++ TuotekuvastoApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using TuotekuvastoApp.Models;
+using Newtonsoft.Json;
 
 namespace TuotekuvastoApp.Controllers;
 
@@ -21,6 +22,19 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         return View();
+    }
+
+    // Tuotejuttuja
+    public IActionResult Product()
+    {
+        var products = GetProducts();
+        return View(products);
+    }
+
+    private List<Product> GetProducts()
+    {
+        var json = System.IO.File.ReadAllText("wwwroot/products.json");
+        return JsonConvert.DeserializeObject<List<Product>>(json) ?? new List<Product>();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
